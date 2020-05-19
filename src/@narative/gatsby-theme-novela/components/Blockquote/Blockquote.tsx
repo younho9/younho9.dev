b3a4f7fcf9a8d485e0003918aa7e1446a53c69ab
Younho Choo <younho9.choo@gmail.com>
2020-05-19 16:20:56 +0900

    Customize article theme (#4)
    * Add .prettierrc
    
    * Customize article theme (#3)
    
    * Modify list padding
    
    * Change Blockquote color
    
    * Modify article width
    
    * Change Blockquote font-style

--- src/@narative/gatsby-theme-novela/components/Blockquote/Blockquote.tsx
+++ src/@narative/gatsby-theme-novela/components/Blockquote/Blockquote.tsx
@@ -3,33 +3,32 @@ import mediaqueries from "@styles/media";
 
 const Blockquote = styled.blockquote`
   transition: ${p => p.theme.colorModeTransition};
-  margin: 15px auto 50px;
+  margin: 20px auto 30px;
   color: ${p => p.theme.colors.articleText};
   font-family: ${p => p.theme.fonts.serif};
-  font-style: italic;
 
   ${mediaqueries.tablet`
-    margin: 10px auto 35px;
+    margin: 20px auto 30px;
   `};
 
   & > p {
     font-family: ${p => p.theme.fonts.serif};
-    max-width: 880px !important;
-    padding-right: 100px;
+    max-width: 680px !important;
     padding-bottom: 0;
+    color: ${p => p.theme.colors.grey};
     width: 100%;
     margin: 0 auto;
-    font-size: 36px;
+    font-size: 24px;
     line-height: 1.32;
     font-weight: bold;
 
     ${mediaqueries.tablet`
-      font-size: 26px;
-      padding: 0 180px;
+      font-size: 25px;
+      max-width: 486px !important;
     `};
 
     ${mediaqueries.phablet`
-      font-size: 36px;
+      font-size: 20px;
       padding: 0 20px 0 40px;
     `};
   }
