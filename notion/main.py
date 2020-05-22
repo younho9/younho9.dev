c26aa3b473601640ef3c2a004d6a2cc6d2806bb7
younho9 <younho9.choo@gmail.com>
2020-05-22 14:24:29 +0900

    Get token from environment variable

--- notion/main.py
+++ notion/main.py
@@ -1,5 +1,5 @@
-import config
 import argparse
+import os
 import sys
 
 from notion_to_blog import get_contents_from_notion
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     description = "Export blog contents from Notion"
     parser = argparse.ArgumentParser(description=description)
 
-    url = config.url
-    token = config.token_v2
+    url = "https://www.notion.so/younho9/Blog-post-younho9-dev-f48c98cb9344475f9b720956b2b72f99"
+    token = os.environ.get("NOTION_TOKEN")
 
     get_contents_from_notion(token, url)
     
